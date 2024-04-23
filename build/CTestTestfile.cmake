c2d2f6a5cc1fa3373d0250891852022ba3947ebd
mercerebo1 <mercerebo1@gmail.com>
2024-04-23 21:49:49 +0200

    initial commit

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/merce/cartesian-controllers
-# Build directory: /home/merce/cartesian-controllers/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libraries")
