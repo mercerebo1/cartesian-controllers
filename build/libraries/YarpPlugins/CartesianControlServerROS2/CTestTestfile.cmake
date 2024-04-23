ae85856c9bdccd9f111d1fe5c03209dbf34256cf
mercerebo1 <mercerebo1@gmail.com>
2024-04-23 21:48:17 +0200

    first upload

---
+++ build/libraries/YarpPlugins/CartesianControlServerROS2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/merce/cartesian-controllers/libraries/YarpPlugins/CartesianControlServerROS2
+# Build directory: /home/merce/cartesian-controllers/build/libraries/YarpPlugins/CartesianControlServerROS2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
