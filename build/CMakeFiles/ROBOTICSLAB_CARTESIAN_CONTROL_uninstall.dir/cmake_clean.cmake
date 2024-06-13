27fe4f3dadb1a8c7fdc735eb0860526b02ce00d5
mercerebo1 <mercerebo1@gmail.com>
2024-06-13 18:21:07 +0200

    delete build directory

--- build/CMakeFiles/ROBOTICSLAB_CARTESIAN_CONTROL_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ROBOTICSLAB_CARTESIAN_CONTROL_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ROBOTICSLAB_CARTESIAN_CONTROL_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
